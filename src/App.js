48c24644e44010017772bbcc91861637cbc156fe
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-12-28 09:16:51 -0200

    Bring data from card.css

--- src/App.js
+++ src/App.js
@@ -7,21 +7,29 @@ import db from './firebase/init'
 // Components
 import './App.css';
 
-const img = "https://secure.gravatar.com/avatar/cc84b9f7a53cf2e0722aea4fb609e1fe.jpg?s=512&d=https%3A%2F%2Fa.slack-edge.com%2F7fa9%2Fimg%2Favatars%2Fava_0021-512.png"
-const comentary = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-
 class App extends Component {
+  state = {
+    comments:[]
+  }
+
+  async componentDidMount () {
+    db.collection('namastop').get().then(users => {
+      let array = []
+      users.docs.forEach(doc => {array.push(doc.data())})
+      this.setState({comments:array})
+    })  
+  }
 
   
   render() {
+    const {comments} = this.state
+    console.log(comments)
     return (
         <React.Fragment>
           <Header />
           <Filter />
           <div className='containers-card'>
-            <Card imgWhoReceived={img} imgWhoSend={img} commentary={comentary} />
-            <Card imgWhoReceived={img} imgWhoSend={img} commentary={comentary} />
-
+          {comments && comments.map((d,idx) => <Card imgWhoReceived={d.photo} imgWhoSend={d.photo} commentary={d.text} key={idx} /> )}
           </div>
         </React.Fragment>
     );
