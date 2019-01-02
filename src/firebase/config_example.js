9f22d9b884c7ef096a912b17a0a1b745f0da9bed
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2019-01-02 21:49:50 -0200

    Update gitignore with env

--- src/firebase/config_example.js
+++
@@ -1,8 +0,0 @@
-export const configFirebase = {
-  apiKey: "data from configuration firebase",
-  authDomain: "data from configuration firebase",
-  databaseURL: "data from configuration firebase",
-  projectId: "data from configuration firebase",
-  storageBucket: "",
-  messagingSenderId: "data from configuration firebase"
-}
